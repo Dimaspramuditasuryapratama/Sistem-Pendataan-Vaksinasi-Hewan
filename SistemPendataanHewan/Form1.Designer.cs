37ad4d6d0647c96a489b948c00049244162b280b
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-04-20 07:07:48 +0700

    Add project files.

---
+++ SistemPendataanHewan/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SistemPendataanHewan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIDPemilik = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.txtNamaPemilik = new System.Windows.Forms.TextBox();
+            this.txtNoHP = new System.Windows.Forms.TextBox();
+            this.txtRTRW = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Pemilik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Alamat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No.HP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "RT/RW";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Pemilik";
+            // 
+            // txtIDPemilik
+            // 
+            this.txtIDPemilik.Location = new System.Drawing.Point(193, 22);
+            this.txtIDPemilik.Multiline = true;
+            this.txtIDPemilik.Name = "txtIDPemilik";
+            this.txtIDPemilik.Size = new System.Drawing.Size(258, 30);
+            this.txtIDPemilik.TabIndex = 5;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(193, 112);
+            this.txtAlamat.Multiline = true;
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(258, 30);
+            this.txtAlamat.TabIndex = 6;
+            // 
+            // txtNamaPemilik
+            // 
+            this.txtNamaPemilik.Location = new System.Drawing.Point(193, 68);
+            this.txtNamaPemilik.Multiline = true;
+            this.txtNamaPemilik.Name = "txtNamaPemilik";
+            this.txtNamaPemilik.Size = new System.Drawing.Size(258, 30);
+            this.txtNamaPemilik.TabIndex = 7;
+            // 
+            // txtNoHP
+            // 
+            this.txtNoHP.Location = new System.Drawing.Point(193, 156);
+            this.txtNoHP.Multiline = true;
+            this.txtNoHP.Name = "txtNoHP";
+            this.txtNoHP.Size = new System.Drawing.Size(258, 30);
+            this.txtNoHP.TabIndex = 8;
+            // 
+            // txtRTRW
+            // 
+            this.txtRTRW.Location = new System.Drawing.Point(193, 202);
+            this.txtRTRW.Multiline = true;
+            this.txtRTRW.Name = "txtRTRW";
+            this.txtRTRW.Size = new System.Drawing.Size(258, 30);
+            this.txtRTRW.TabIndex = 9;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(657, 22);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(177, 30);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Membuat Koneksi";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(657, 68);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(177, 30);
+            this.btnLoad.TabIndex = 11;
+            this.btnLoad.Text = "Menampilkan Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(657, 112);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(177, 30);
+            this.btnInsert.TabIndex = 12;
+            this.btnInsert.Text = "Menambah Data";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(657, 156);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(177, 30);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Mengubah Data";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(657, 202);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(177, 30);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Menghapus Data";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1304, 189);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtRTRW);
+            this.Controls.Add(this.txtNoHP);
+            this.Controls.Add(this.txtNamaPemilik);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtIDPemilik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "Form Data Pemilik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIDPemilik;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.TextBox txtNamaPemilik;
+        private System.Windows.Forms.TextBox txtNoHP;
+        private System.Windows.Forms.TextBox txtRTRW;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Vaksinasi/Form2.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemPendataanHewan
+{
+    public partial class FormVaksinasi : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBHewanPeliharaanADO;Integrated Security=True";
+
+        public FormVaksinasi()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                MessageBox.Show("Koneksi berhasil!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtIDVaksinasi.Clear();
+            txtIDHewan.Clear();
+            txtJenisVaksin.Clear();
+            dtpTanggalVaksin.Value = DateTime.Now;
+            dtpTanggalBerikutnya.Value = DateTime.Now;
+            cmbStatusVaksin.SelectedIndex = -1;
+            txtKeterangan.Clear();
+            txtIDHewan.Focus();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Columns.Add("IDVaksinasi", "ID Vaksinasi");
+                dataGridView1.Columns.Add("IDHewan", "ID Hewan");
+                dataGridView1.Columns.Add("JenisVaksin", "Jenis Vaksin");
+                dataGridView1.Columns.Add("TanggalVaksin", "Tanggal Vaksin");
+                dataGridView1.Columns.Add("TanggalBerikutnya", "Tanggal Berikutnya");
+                dataGridView1.Columns.Add("StatusVaksin", "Status Vaksin");
+                dataGridView1.Columns.Add("Keterangan", "Keterangan");
+
+                string query = "SELECT * FROM Vaksinasi";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["IDVaksinasi"].ToString(),
+                        reader["IDHewan"].ToString(),
+                        reader["JenisVaksin"].ToString(),
+                        Convert.ToDateTime(reader["TanggalVaksin"]).ToShortDateString(),
+                        Convert.ToDateTime(reader["TanggalBerikutnya"]).ToShortDateString(),
+                        reader["StatusVaksin"].ToString(),
+                        reader["Keterangan"].ToString()
+                    );
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIDHewan.Text == "")
+                {
+                    MessageBox.Show("ID Hewan harus diisi!");
+                    txtIDHewan.Focus();
+                    return;
+                }
+
+                if (txtJenisVaksin.Text == "")
+                {
+                    MessageBox.Show("Jenis Vaksin harus diisi!");
+                    txtJenisVaksin.Focus();
+                    return;
+                }
+
+                if (cmbStatusVaksin.Text == "")
+                {
+                    MessageBox.Show("Status Vaksin harus dipilih!");
+                    cmbStatusVaksin.Focus();
+                    return;
+                }
+
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                string query = @"INSERT INTO Vaksinasi
+                                (IDHewan, JenisVaksin, TanggalVaksin, TanggalBerikutnya, StatusVaksin, Keterangan)
+                                VALUES
+                                (@IDHewan, @JenisVaksin, @TanggalVaksin, @TanggalBerikutnya, @StatusVaksin, @Keterangan)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDHewan", txtIDHewan.Text);
+                cmd.Parameters.AddWithValue("@JenisVaksin", txtJenisVaksin.Text);
+                cmd.Parameters.AddWithValue("@TanggalVaksin", dtpTanggalVaksin.Value.Date);
+                cmd.Parameters.AddWithValue("@TanggalBerikutnya", dtpTanggalBerikutnya.Value.Date);
+                cmd.Parameters.AddWithValue("@StatusVaksin", cmbStatusVaksin.Text);
+                cmd.Parameters.AddWithValue("@Keterangan", txtKeterangan.Text);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data berhasil ditambahkan.");
+                    LoadData();
+                    ClearForm();
+                }
+                else
+                {
+                    MessageBox.Show("Data gagal ditambahkan.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIDVaksinasi.Text == "")
+                {
+                    MessageBox.Show("Pilih data pada tabel terlebih dahulu!");
+                    return;
+                }
+
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                string query = @"UPDATE Vaksinasi
+                                 SET IDHewan = @IDHewan,
+                                     JenisVaksin = @JenisVaksin,
+                                     TanggalVaksin = @TanggalVaksin,
+                                     TanggalBerikutnya = @TanggalBerikutnya,
+                                     StatusVaksin = @StatusVaksin,
+                                     Keterangan = @Keterangan
+                                 WHERE IDVaksinasi = @IDVaksinasi";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDVaksinasi", txtIDVaksinasi.Text);
+                cmd.Parameters.AddWithValue("@IDHewan", txtIDHewan.Text);
+                cmd.Parameters.AddWithValue("@JenisVaksin", txtJenisVaksin.Text);
+                cmd.Parameters.AddWithValue("@TanggalVaksin", dtpTanggalVaksin.Value.Date);
+                cmd.Parameters.AddWithValue("@TanggalBerikutnya", dtpTanggalBerikutnya.Value.Date);
+                cmd.Parameters.AddWithValue("@StatusVaksin", cmbStatusVaksin.Text);
+                cmd.Parameters.AddWithValue("@Keterangan", txtKeterangan.Text);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data berhasil diubah.");
+                    LoadData();
+                    ClearForm();
+                }
+                else
+                {
+                    MessageBox.Show("Data tidak ditemukan.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIDVaksinasi.Text == "")
+                {
+                    MessageBox.Show("Pilih data pada tabel terlebih dahulu!");
+                    return;
+                }
+
+                DialogResult konfirmasi = MessageBox.Show(
+                    "Yakin ingin menghapus data ini?",
+                    "Konfirmasi",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (konfirmasi == DialogResult.Yes)
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+
+                    string query = "DELETE FROM Vaksinasi WHERE IDVaksinasi = @IDVaksinasi";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@IDVaksinasi", txtIDVaksinasi.Text);
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Data berhasil dihapus.");
+                        LoadData();
+                        ClearForm();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data tidak ditemukan.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txtIDVaksinasi.Text = row.Cells["IDVaksinasi"].Value?.ToString();
+                txtIDHewan.Text = row.Cells["IDHewan"].Value?.ToString();
+                txtJenisVaksin.Text = row.Cells["JenisVaksin"].Value?.ToString();
+                dtpTanggalVaksin.Value = Convert.ToDateTime(row.Cells["TanggalVaksin"].Value);
+                dtpTanggalBerikutnya.Value = Convert.ToDateTime(row.Cells["TanggalBerikutnya"].Value);
+                cmbStatusVaksin.Text = row.Cells["StatusVaksin"].Value?.ToString();
+                txtKeterangan.Text = row.Cells["Keterangan"].Value?.ToString();
+            }
+        }
+
+        private void FormVaksinasi_Load(object sender, EventArgs e)
+        {
+            cmbStatusVaksin.Items.Clear();
+            cmbStatusVaksin.Items.Add("Sudah");
+            cmbStatusVaksin.Items.Add("Belum");
+            cmbStatusVaksin.Items.Add("Ulang");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+    }
+}
