a05e4e41aa15c504de494824fac55ebf69717f42
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 03:11:58 +0700

    feat: buat Form8 dengan koneksi database, sembunyikan btnLaporan untuk Petugas, dan panggil HitungTotalHewan

---
+++ SistemPendataanHewan/Form8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemPendataanHewan
+{
+    public partial class Form8 : Form
+    {
+        private readonly string connectionString =
+            "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBHewanPeliharaanADO;Integrated Security=True";
+
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+            // 1. Batasan Hak Akses Petugas
+            if (SesiPengguna.RoleUser == "Petugas")
+            {
+                btnLaporan.Visible = false;
+            }
+
+            // 2. Jalankan Fungsi ExecuteScalar untuk Menghitung Total Data
+            HitungTotalHewan();
+        }
+    }
+}
