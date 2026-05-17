b9c25225e66257a6795bd60d4da9350913720882
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 02:56:22 +0700

    feat: inisialisasi Form5 dengan DataGridView dan ComboBox

---
+++ SistemPendataanHewan/Form5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemPendataanHewan
+{
+    public partial class Form5 : Form
+    {
+        private readonly string connectionString =
+            "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBHewanPeliharaanADO;Integrated Security=True";
+
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            cmbJenisKelamin.Items.Clear();
+            cmbJenisKelamin.Items.Add("L");
+            cmbJenisKelamin.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Kunci tombol Hapus untuk Petugas
+            if (SesiPengguna.RoleUser == "Petugas")
+            {
+                btnDelete.Visible = false;
+            }
+        }
+    }
+}
