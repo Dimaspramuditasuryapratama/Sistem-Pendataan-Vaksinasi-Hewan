db2476703c894462d31f1370fb81fb04b02bb3b7
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 04:53:09 +0700

    feat(ui): layout Form5 dengan input ID Hewan, ID Pemilik, Nama, Jenis, Ras, Jenis Kelamin, Umur, Warna, dan 5 tombol CRUD

---
+++ SistemPendataanHewan/Form5.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace SistemPendataanHewan
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtIDHewan = new System.Windows.Forms.TextBox();
+            this.txtIDPemilik = new System.Windows.Forms.TextBox();
+            this.txtNamaHewan = new System.Windows.Forms.TextBox();
+            this.txtJenisHewan = new System.Windows.Forms.TextBox();
+            this.txtUmur = new System.Windows.Forms.TextBox();
+            this.txtRas = new System.Windows.Forms.TextBox();
+            this.txtWarna = new System.Windows.Forms.TextBox();
+            this.cmbJenisKelamin = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Hewan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID Pemilik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nama Hewan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label4.Location = new System.Drawing.Point(30, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jenis Hewan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label5.Location = new System.Drawing.Point(30, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ras";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label6.Location = new System.Drawing.Point(30, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jenis Kelamin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label7.Location = new System.Drawing.Point(30, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Umur";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label8.Location = new System.Drawing.Point(30, 310);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Warna";
+            // 
+            // txtIDHewan
+            // 
+            this.txtIDHewan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIDHewan.Location = new System.Drawing.Point(170, 28);
+            this.txtIDHewan.Name = "txtIDHewan";
+            this.txtIDHewan.Size = new System.Drawing.Size(280, 25);
+            this.txtIDHewan.TabIndex = 8;
+            this.txtIDHewan.TextChanged += new System.EventHandler(this.txtIDHewan_TextChanged);
+            // 
+            // txtIDPemilik
+            // 
+            this.txtIDPemilik.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIDPemilik.Location = new System.Drawing.Point(170, 68);
+            this.txtIDPemilik.Name = "txtIDPemilik";
+            this.txtIDPemilik.Size = new System.Drawing.Size(280, 25);
+            this.txtIDPemilik.TabIndex = 9;
+            // 
+            // txtNamaHewan
+            // 
+            this.txtNamaHewan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNamaHewan.Location = new System.Drawing.Point(170, 108);
+            this.txtNamaHewan.Name = "txtNamaHewan";
+            this.txtNamaHewan.Size = new System.Drawing.Size(280, 25);
+            this.txtNamaHewan.TabIndex = 10;
+            // 
+            // txtJenisHewan
+            // 
+            this.txtJenisHewan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtJenisHewan.Location = new System.Drawing.Point(170, 148);
+            this.txtJenisHewan.Name = "txtJenisHewan";
+            this.txtJenisHewan.Size = new System.Drawing.Size(280, 25);
+            this.txtJenisHewan.TabIndex = 11;
+            // 
+            // txtRas
+            // 
+            this.txtRas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRas.Location = new System.Drawing.Point(170, 188);
+            this.txtRas.Name = "txtRas";
+            this.txtRas.Size = new System.Drawing.Size(280, 25);
+            this.txtRas.TabIndex = 13;
+            // 
+            // cmbJenisKelamin
+            // 
+            this.cmbJenisKelamin.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbJenisKelamin.FormattingEnabled = true;
+            this.cmbJenisKelamin.Items.AddRange(new object[] {
+            "L",
+            "P"});
+            this.cmbJenisKelamin.Location = new System.Drawing.Point(170, 227);
+            this.cmbJenisKelamin.Name = "cmbJenisKelamin";
+            this.cmbJenisKelamin.Size = new System.Drawing.Size(280, 25);
+            this.cmbJenisKelamin.TabIndex = 16;
+            // 
+            // txtUmur
+            // 
+            this.txtUmur.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUmur.Location = new System.Drawing.Point(170, 268);
+            this.txtUmur.Name = "txtUmur";
+            this.txtUmur.Size = new System.Drawing.Size(280, 25);
+            this.txtUmur.TabIndex = 12;
+            // 
+            // txtWarna
+            // 
+            this.txtWarna.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtWarna.Location = new System.Drawing.Point(170, 308);
+            this.txtWarna.Name = "txtWarna";
+            this.txtWarna.Size = new System.Drawing.Size(280, 25);
+            this.txtWarna.TabIndex = 15;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnConnect.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(520, 28);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(180, 40);
+            this.btnConnect.TabIndex = 17;
+            this.btnConnect.Text = "Membuka Koneksi";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnLoad.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(520, 78);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(180, 40);
+            this.btnLoad.TabIndex = 18;
+            this.btnLoad.Text = "Menampilkan Data";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.LightGreen;
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(520, 128);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(180, 40);
+            this.btnInsert.TabIndex = 19;
+            this.btnInsert.Text = "Menambah Data";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(520, 178);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(180, 40);
+            this.btnUpdate.TabIndex = 20;
+            this.btnUpdate.Text = "Mengubah Data";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(520, 228);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(180, 40);
+            this.btnDelete.TabIndex = 21;
+            this.btnDelete.Text = "Menghapus Data";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 360);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(670, 180);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbJenisKelamin);
+            this.Controls.Add(this.txtWarna);
+            this.Controls.Add(this.txtRas);
+            this.Controls.Add(this.txtUmur);
+            this.Controls.Add(this.txtJenisHewan);
+            this.Controls.Add(this.txtNamaHewan);
+            this.Controls.Add(this.txtIDPemilik);
+            this.Controls.Add(this.txtIDHewan);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Data Hewan";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtIDHewan;
+        private System.Windows.Forms.TextBox txtIDPemilik;
+        private System.Windows.Forms.TextBox txtNamaHewan;
+        private System.Windows.Forms.TextBox txtJenisHewan;
+        private System.Windows.Forms.TextBox txtUmur;
+        private System.Windows.Forms.TextBox txtRas;
+        private System.Windows.Forms.TextBox txtWarna;
+        private System.Windows.Forms.ComboBox cmbJenisKelamin;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
