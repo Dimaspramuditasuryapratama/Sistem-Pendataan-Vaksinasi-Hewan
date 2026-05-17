01672aeb1e493e19417b54252c3b07c16afdb698
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 02:34:56 +0700

    feat: inisialisasi Form10 dan setup DataGridView

---
+++ SistemPendataanHewan/Form10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemPendataanHewan
+{
+    public partial class Form10 : Form
+    {
+        private readonly string connectionString =
+            "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBHewanPeliharaanADO;Integrated Security=True";
+
+        public Form10()
+        {
+            InitializeComponent();
+        }
+
+        private void Form10_Load(object sender, EventArgs e)
+        {
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
