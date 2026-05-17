6b04f4e8bc03121c8725d00f70a57e27387331b9
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 05:23:53 +0700

    feat: panggil sp_CountPemilik dengan output parameter dan tampilkan total di judul form

--- SistemPendataanHewan/Form1.cs
+++ SistemPendataanHewan/Form1.cs
@@ -47,5 +47,32 @@ namespace SistemPendataanHewan
             txtNamaPemilik.Focus();
         }
 
+        private void HitungTotal()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("sp_CountPemilik", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        SqlParameter outputParam = new SqlParameter("@Total", SqlDbType.Int);
+                        outputParam.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(outputParam);
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+
+                        this.Text = "Form Data Pemilik - Total Data: " + outputParam.Value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menghitung total: " + ex.Message);
+            }
+        }
+
     }
 }
