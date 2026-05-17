a28077e56ce5c2022afea45e93bec824d11dd08b
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 02:46:49 +0700

    feat: inisialisasi Form4 login dengan password masking

---
+++ SistemPendataanHewan/Form4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemPendataanHewan
+{
+    public partial class Form4 : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-2QET043V\\DIMAS;Initial Catalog=DBHewanPeliharaanADO;Integrated Security=True";
+
+        public Form4()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            // Password akan tampil sebagai *****
+            txtPassword.UseSystemPasswordChar = true;
+        }
+    }
+}
