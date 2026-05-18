1d0df533bacd6d71f9ab1986b3805d4eb878a3ba
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-05-18 07:39:17 +0700

    feat: tambah HitungTotal dengan output parameter @Total dan tampilkan di judul form

--- SistemPendataanHewan/Form9.cs
+++ SistemPendataanHewan/Form9.cs
@@ -49,5 +49,32 @@ namespace SistemPendataanHewan
             txtIDHewan.Focus();
         }
 
+        private void HitungTotal()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("sp_CountVaksinasi", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        SqlParameter outputParam = new SqlParameter("@Total", SqlDbType.Int);
+                        outputParam.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(outputParam);
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+
+                        this.Text = "Form Data Vaksinasi - Total Data: " + outputParam.Value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menghitung total: " + ex.Message);
+            }
+        }
+
     }
 }
